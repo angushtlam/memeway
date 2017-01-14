f4cbfb6fafe5e3e910407f2dde90054839ea5ce4
Dan Starner <starner.daniel5@gmail.com>
2017-01-14 09:36:32 -0500

    Rater and Index view

--- memeway/urls.py
+++ memeway/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
 
 urlpatterns = [
+    url(r'^$', index, name="index"),
     url(r'^admin/', admin.site.urls),
 ]
