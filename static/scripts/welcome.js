0fdb621f86f1823f995bf83411083e3e5053802b
Angus Lam <angushtlam@gmail.com>
2017-01-14 20:02:48 -0500

    Welcome page client js

---
+++ static/scripts/welcome.js
@@ -0,0 +1,56 @@
+var grid = $('.meme-grid');
+
+grid.masonry({
+  itemSelector: '.meme',
+  columnWidth: '.grid-sizer',
+	transitionDuration: 0,
+  gutter: 15
+});
+
+grid.imagesLoaded().progress(function() {
+  grid.masonry('layout');
+});
+
+function attachToMemeItem(jqueryObj) {
+	$(jqueryObj).click(function () {
+		$(this).toggleClass('selected');
+	});
+};
+
+$(document).ready(function () {
+	$('.meme').each(function () {
+		attachToMemeItem(this);
+	});
+
+	$('.fab-continue').click(function () {
+		var selectedMemes = $('.meme.selected');
+		if (selectedMemes.length < 5) {
+			alert('Pick at least 5!');
+		} else {
+			var missingResponses = selectedMemes.length;
+
+			selectedMemes.each(function () {
+				var memeId = $(this).attr('meme-id');
+
+				$.ajax({
+				  type: 'POST',
+				  url: '/discover/save-meme',
+				  data: {
+				  	'image-id': 'memeId'
+				  },
+				  error: function (xhr, status, errorThrown) {
+				  	alert(errorThrown);
+				  },
+				  success: function () {
+				  	missingResponses--;
+
+				  	if (missingResponses < 1) {
+							window.location.replace('/discover');
+				  	}
+				  },
+				  dataType: 'json'
+				});
+			});
+		}
+	});
+});
