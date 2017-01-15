c0f2b49a62bbd8ce78a3163abce97b9c8f98b25a
Angus Lam <angushtlam@gmail.com>
2017-01-14 22:05:50 -0500

    Add memes from other people's profiles

--- static/scripts/discover/discover.js
+++ static/scripts/discover/discover.js
@@ -10,3 +10,58 @@ grid.masonry({
 grid.imagesLoaded().progress(function() {
   grid.masonry('layout');
 });
+
+function attachToMemeItem(jqueryObj) {
+	$(jqueryObj).click(function () {
+		$(this).toggleClass('selected');
+
+		var selectedMemes = $('.meme.selected');
+
+		if (selectedMemes.length > 0) {
+			$('.fab-continue').removeClass('disabled');
+		} else {
+			$('.fab-continue').addClass('disabled');
+		}
+	});
+};
+
+$(document).ready(function () {
+	$('.meme').each(function () {
+		attachToMemeItem(this);
+	});
+
+	$('.fab-continue').click(function () {
+		var selectedMemes = $('.meme.selected');
+		if (selectedMemes.length < 1) {
+			alert('You did not select any memes!');
+		} else {
+			var data = [];
+
+			selectedMemes.each(function () {
+				var memeId = $(this).data('meme-id');
+				data.push({'image_id': memeId});
+			});
+
+
+			$.ajax({
+				  type: 'POST',
+				  url: '/discover/save-meme',
+				  data: JSON.stringify(data),
+				  error: function (xhr, status, errorThrown) {
+				  	// alert(errorThrown);
+				  },
+				  success: function () {	
+						var selectedMemes = $('.meme.selected');		
+						selectedMemes.each(function () {
+							$(this).removeClass('selected');
+						});					
+
+						alert('You have added the selected memes to your profile.');
+						$('.fab-continue').addClass('disabled');
+				  },
+				  dataType: 'text',
+				  contentType: 'application/json'
+				});
+		}
+	});
+});
