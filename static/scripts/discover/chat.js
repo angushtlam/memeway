8a81bdf2d1639f2d1af040d3dc3f1a6f966cd3c3
Angus Lam <angushtlam@gmail.com>
2017-01-15 17:12:49 -0500

    Chat automatically updates

--- static/scripts/discover/chat.js
+++ static/scripts/discover/chat.js
@@ -1,9 +1,22 @@
+function refreshPage() {
+    if ($('#chat-message-field').val() == "") {
+       window.location.reload(1);
+    }
+
+    setTimeout(refreshPage(), 5000);
+}
+
 $(document).ready(function () {
     if (!(document.referrer.includes('/discover/chat'))) {
         $('.user-contacts').addClass('animated slideInDown');
     }
 
     $('.chat').scrollTop($(".chat")[0].scrollHeight);
+
+
+    setTimeout(function(){
+        refreshPage();
+    }, 5000);
 });
 
 $('#chat-input-form').submit(function (event) {
