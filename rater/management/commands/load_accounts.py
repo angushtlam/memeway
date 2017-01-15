46b39d7f160eac9c8fe9d15b90d57bf90ba0aabd
Dan Starner <starner.daniel5@gmail.com>
2017-01-15 15:03:26 -0500

    Chat bot set

--- rater/management/commands/load_accounts.py
+++ rater/management/commands/load_accounts.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from memeway import random_generator
-from rater.models import Meme, MemeImage, User
+from rater.models import Meme, MemeImage, User, ChatRoom
 import random
 
 
@@ -16,7 +16,12 @@ class Command(BaseCommand):
             if user.username not in ["dan", "angus"]:
                 user.delete()
 
+        for chat in ChatRoom.objects.all():
+            chat.delete()
+
         accounts = options['accounts']
+
+        users = []
 
         for num in range(0, accounts):
 
@@ -29,8 +34,29 @@ class Command(BaseCommand):
 
             user.save()
 
-            random_images = random.sample(list(MemeImage.objects.all()), 6)
+            users.append(user)
+
+            random_images = random.sample(list(MemeImage.objects.all()), 9)
 
             for image in random_images:
                 image.users_who_liked.add(user)
                 image.save()
+
+        # Make connections
+        for user in users:
+            random_users = random.sample(list(User.objects.all()), int(accounts/2))
+
+            for rand_user in random_users:
+                if user != rand_user:
+                    user.liked.add(rand_user)
+                    user.save()
+
+                    if user in rand_user.liked.all():
+                        user.matches.add(rand_user)
+                        rand_user.matches.add(user)
+                        rand_user.save()
+                        user.save()
+                        chat = ChatRoom.objects.create()
+                        chat.users.add(rand_user)
+                        chat.users.add(user)
+                        chat.save()
