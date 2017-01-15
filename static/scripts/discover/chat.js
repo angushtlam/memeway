f32fbd4474c46a748de2c23100bb48a94beebd17
Angus Lam <angushtlam@gmail.com>
2017-01-15 15:05:02 -0500

    Nonowrking chat script

--- static/scripts/discover/chat.js
+++ static/scripts/discover/chat.js
@@ -5,3 +5,34 @@ $(document).ready(function () {
 
 	$('.chat').scrollTop($(".chat")[0].scrollHeight);
 });
+
+$('#chat-input-form').submit(function (event) {
+	if ($("#talking-to").html() == "memecat") {
+		var formId = this.id,
+        form = this;
+
+    event.preventDefault();
+
+    var chatKey = $('#chat-key').html();
+
+    var data = {
+    	text: $('#chat-message-field').val(),
+    	chat_key: chatKey
+    };
+
+    $.ajax({
+		  type: 'POST',
+		  url: 'http://127.0.0.1:5000',
+		  data: JSON.stringify(data),
+		  error: function (xhr, status, errorThrown) {
+		  	alert(errorThrown);
+		  },
+		  success: function () {
+				location.reload();
+		  },
+		  dataType: 'text',
+		  contentType: 'application/json'
+		});
+
+	}
+});
